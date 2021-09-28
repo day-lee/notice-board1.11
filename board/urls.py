53681085c97bd1e41f7831792339212a56df34ca
Dayeon Lee <iamdayeonlee.com>
2021-09-28 15:56:12 +0900

    Remove django 3.2 version urls (left for reference)

--- board/urls.py
+++ board/urls.py
@@ -12,15 +12,9 @@ app_name = 'board'
 
 urlpatterns = [
 		url(r'^$', PostListView.as_view() , name='notice-board' ),
-
-		# url(r'^posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 		url(r'^posts/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
 		url(r'^posts/new', PostCreateView.as_view(), name='post-create'),
-
-	    # url(r'^posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
 		url(r'^posts/(?P<pk>\d+)/edit/', PostUpdateView.as_view(), name='post-update'),
-
-		#url(r'^posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 	    url(r'^posts/(?P<pk>\d+)/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 		url(r'^posts/js', PostTestView.as_view(), name='post-test'),
