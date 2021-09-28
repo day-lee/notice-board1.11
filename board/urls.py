237f4b4e0668276d22b410f4454ab1211d0223dd
Dayeon Lee <iamdayeonlee.com>
2021-09-28 15:57:58 +0900

    Fix indentation

--- board/urls.py
+++ board/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
 		url(r'^posts/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
 		url(r'^posts/new', PostCreateView.as_view(), name='post-create'),
 		url(r'^posts/(?P<pk>\d+)/edit/', PostUpdateView.as_view(), name='post-update'),
-	    url(r'^posts/(?P<pk>\d+)/delete/', PostDeleteView.as_view(), name='post-delete'),
+		url(r'^posts/(?P<pk>\d+)/delete/', PostDeleteView.as_view(), name='post-delete'),
 		url(r'^posts/js', PostTestView.as_view(), name='post-test'),
 ]
