c1b23f946b7f26aa6ebd4c834bac692b71b44194
Dayeon Lee <iamdayeonlee.com>
2021-09-28 15:58:56 +0900

    Fix indentation

--- board/urls.py
+++ board/urls.py
@@ -1,10 +1,10 @@
 #from django.urls import path
 from django.conf.urls import url
 from .views import (PostListView,
-                    PostDetailView,
+					PostDetailView,
 					PostCreateView,
-                    PostUpdateView,
-                    PostDeleteView,
+					PostUpdateView,
+					PostDeleteView,
 					PostTestView
                    )
 
