a678d878d3f6ddb005a4343c8c6c0c66cc350d9f
Dayeon Lee <iamdayeonlee.com>
2021-10-05 17:29:30 +0900

    Add all to edx-platform

--- board/urls.py
+++ board/urls.py
@@ -11,7 +11,7 @@ from .views import (PostListView,
 app_name = 'board'
 
 urlpatterns = [
-		url(r'^$', PostListView.as_view() , name='notice-board' ),
+		url(r'^$', PostListView.as_view(), name='notice-board'),
 		url(r'^posts/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
 		url(r'^posts/new', PostCreateView.as_view(), name='post-create'),
 		url(r'^posts/(?P<pk>\d+)/edit/', PostUpdateView.as_view(), name='post-update'),
